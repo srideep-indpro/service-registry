4003a4149312364b3b4bd1a422d823a572a629b8
unknown <cogjava2607@iiht.tech>
2023-02-16 11:22:13 +0530

    Added initial files

---
+++ src/test/java/com/vlabs/lms/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlabs.lms.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
